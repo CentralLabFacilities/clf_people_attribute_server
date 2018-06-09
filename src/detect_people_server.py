e3ab4bee8ddbf3e6ea57733a38666bd8510f21e2
Felix Friese <ffriese@techfak.uni-bielefeld.de>
2018-06-09 22:23:32 +0200

    transform to base_footprint

--- src/detect_people_server.py
+++ src/detect_people_server.py
@@ -11,7 +11,9 @@ from clf_perception_vision_msgs.srv import LearnPersonImage, DoIKnowThatPersonIm
 from gender_and_age_msgs.srv import GenderAndAgeService, GenderAndAgeServiceRequest
 from openpose_ros_msgs.msg import PersonAttributesWithPose
 from sensor_msgs.msg import CameraInfo
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseStamped
+
+from tf import TransformListener
 
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import model_wh, get_graph_path
@@ -65,7 +67,7 @@ class ShirtColor:
         for y in range(0, rows - GRID_SIZE, GRID_SIZE):
             for x in range(0, cols - GRID_SIZE, GRID_SIZE):
                 mask = np.full((crop_img.shape[0], crop_img.shape[1]), 0, dtype=np.uint8)
-                cv2.rectangle(mask, (x, y), (x+GRID_SIZE, y+GRID_SIZE), 255, CV_FILLED)
+                cv2.rectangle(mask, (x, y), (x + GRID_SIZE, y + GRID_SIZE), 255, CV_FILLED)
                 bin_colors[ShirtColor.get_pixel_color_type(cv2.mean(hsv_crop_img, mask))] += 1
 
         result_color = "no color"
@@ -150,8 +152,12 @@ class FaceID:
 
 class Helper:
     def __init__(self, depth_info_topic='/pepper_robot/camera/depth/camera_info'):
+
+        self.tf = TransformListener()
         self.depth_sub = rospy.Subscriber(depth_info_topic,
                                           CameraInfo, self.camera_info_callback)
+        self.camera_frame = 'CameraDepth_optical_frame'
+        self.base_frame = 'base_footprint'
         self.cx = None
         self.cy = None
         self.fx = None
@@ -193,23 +199,38 @@ class Helper:
             values.append(value)
 
         depth = np.median(values)
-        point = Point()
+        pose = PoseStamped()
+        pose.pose.position.x = (float(x) - self.cx) * depth * constant_x
+        pose.pose.position.y = (float(y) - self.cy) * depth * constant_y
+        pose.pose.position.z = depth * unit_scaling
+        pose.pose.orientation.w = 1
+        pose.header.stamp = rospy.Time.now()
+        pose.header.frame_id = self.camera_frame
 
-        point.x = (float(x)-self.cx) * depth * constant_x
-        point.y = (float(x)-self.cy) * depth * constant_y
-        point.z = depth * unit_scaling
-        return point
+        if not self.tf.frameExists(self.base_frame):
+            rospy.logwarn('%s does not exist!' % self.base_frame)
+        if not self.tf.frameExists(pose.header.frame_id):
+            rospy.logwarn('%s does not exist!' % str(pose.header.frame_id))
+        if self.tf.frameExists(pose.header.frame_id) and self.tf.frameExists(self.base_frame):
+            try:
+                self.tf.waitForTransform(self.base_frame, self.camera_frame, rospy.Time.now(),
+                                         rospy.Duration(4.0))
+                transformed_pose = self.tf.transformPose(self.base_frame, pose)
+                return transformed_pose
+            except Exception, e:
+                rospy.logerr("Exception %s" % e)
+        return None
 
     @staticmethod
     def head_roi(image, person):
         parts = ['Nose', 'RightEar', 'RightEye', 'LeftEar', 'LeftEye']
         amount = int(sum([np.ceil(person[p]['confidence']) for p in parts]))
         vf = sum([np.ceil(person[p]['y']) for p in parts])
-        v = int(np.floor(vf/amount))
+        v = int(np.floor(vf / amount))
 
         if amount <= 1:
             x = y = w = h = -1
-            return image[y:y+h, x:x+w]
+            return image[y:y + h, x:x + w]
 
         distlist_x = []
         if person['Nose']['x'] != 0:
@@ -232,12 +253,12 @@ class Helper:
         h = w * 1.5
         y = v - h / 2
 
-        if (x + w >= image.shape[1]):
+        if x + w >= image.shape[1]:
             w = w - np.abs((x + w) - image.shape[1])
-        if (y + h >= image.shape[0]):
+        if y + h >= image.shape[0]:
             h = h - np.abs((y + h) - image.shape[0])
 
-        return image[int(y):int(y+h), int(x):int(x+w)], x, y, w, h
+        return image[int(y):int(y + h), int(x):int(x + w)], x, y, w, h
 
     @staticmethod
     def upper_body_roi(image, person):
@@ -246,7 +267,7 @@ class Helper:
 
         if amount <= 1:
             x = y = w = h = -1
-            return image[y:y+h, x:x+w]
+            return image[y:y + h, x:x + w]
 
         if person['LeftShoulder']['confidence'] > 0 and person['RightShoulder']['confidence'] > 0:
             y = person['RightShoulder']['y']
@@ -258,25 +279,26 @@ class Helper:
 
             if person['RightHip']['confidence'] > 0:
                 h = (person['RightHip']['y'] - y)
-            elif (person['LeftHip']['confidence'] > 0):
+            elif person['LeftHip']['confidence'] > 0:
                 h = (person['LeftHip']['y'] - y)
             else:
                 rospy.logerr("no hip found")
                 h = w
         else:
-            if (person['RightHip']['confidence'] > 0 and person['LeftHip']['confidence'] > 0):
-                if ((person['RightShoulder']['confidence'] > 0) ^ (person['LeftShoulder']['confidence'] > 0)):
+            if person['RightHip']['confidence'] > 0 and person['LeftHip']['confidence'] > 0:
+                if (person['RightShoulder']['confidence'] > 0) ^ (person['LeftShoulder']['confidence'] > 0):
                     w = np.abs(person['LeftHip']['x'] - person['RightHip']['x'])
                     y = person['LeftShoulder']['y'] + person['RightShoulder']['y']
-                    if ((person['RightHip']['x'] - person['LeftHip']['x']) < 0):
+                    if (person['RightHip']['x'] - person['LeftHip']['x']) < 0:
                         x = person['RightHip']['x']
-                        h = person['RightHip']['y'] - person['LeftShoulder']['x'] - person['RightShoulder']['x'] #one of the shoulders values will be 0.
+                        h = person['RightHip']['y'] - person['LeftShoulder']['x'] - person['RightShoulder'][
+                            'x']  # one of the shoulders values will be 0.
                     else:
                         x = person['LeftHip']['x']
                         h = person['LeftHip']['y'] - person['LeftShoulder']['x'] - person['RightShoulder']['x']
                 else:
                     w = np.abs(person['LeftHip']['x'] - person['RightHip']['x'])
-                    if ((person['RightHip']['x'] - person['LeftHip']['x']) < 0):
+                    if (person['RightHip']['x'] - person['LeftHip']['x']) < 0:
                         x = person['RightHip']['x']
                         h = w
                         y = person['RightHip']['y'] - h
@@ -285,26 +307,26 @@ class Helper:
                         h = w
                         y = person['LeftHip']['y'] - h
             else:
-                if (person['RightHip']['confidence'] > 0):
-                    if (person['RightShoulder']['confidence'] > 0):
+                if person['RightHip']['confidence'] > 0:
+                    if person['RightShoulder']['confidence'] > 0:
                         x = person['RightShoulder']['x']
                         y = person['RightShoulder']['y']
                         h = np.abs(person['RightShoulder']['y'] - person['RightHip']['y'])
                         w = h * 0.5
-                    
-                    if (person['LeftShoulder']['confidence'] > 0):
+
+                    if person['LeftShoulder']['confidence'] > 0:
                         x = person['RightHip']['x']
                         y = person['LeftShoulder']['y']
                         h = np.abs(person['LeftShoulder']['y'] - person['RightHip']['y'])
                         w = np.abs(person['LeftShoulder']['x'] - person['RightHip']['x'])
-                if (person['LeftHip']['confidence'] > 0):
-                    if (person['RightShoulder']['confidence'] > 0):
+                if person['LeftHip']['confidence'] > 0:
+                    if person['RightShoulder']['confidence'] > 0:
                         x = person['RightShoulder']['x']
                         y = person['RightShoulder']['y']
                         h = np.abs(person['RightShoulder']['y'] - person['LeftHip']['y'])
                         w = h * 0.5
-                    
-                    if (person['LeftShoulder']['confidence'] > 0):
+
+                    if person['LeftShoulder']['confidence'] > 0:
                         y = person['LeftShoulder']['y']
                         h = np.abs(person['LeftShoulder']['y'] - person['LeftHip']['y'])
                         w = h * 0.5
@@ -312,20 +334,20 @@ class Helper:
                 else:
                     rospy.log("No BB possible: RShoulder['confidence'] %f, LShoulder['confidence'] %f,"
                               " RHip['confidence'] %f, LHip['confidence'] %f \n",
-                              person['RightShoulder']['confidence'], person['LeftShoulder']['confidence'], person['RightHip']['confidence'],
+                              person['RightShoulder']['confidence'], person['LeftShoulder']['confidence'],
+                              person['RightHip']['confidence'],
                               person['LeftHip']['confidence'])
 
-
-        if (x + w >= image.shape[1]):
+        if x + w >= image.shape[1]:
             w = w - np.abs((x + w) - image.shape[0])
-        if (y + h >= image.shape[0]):
+        if y + h >= image.shape[0]:
             h = h - np.abs((y + h) - image.shape[1])
 
-        if ((w <= 0) or (h <= 0) or (x <= 0) or (y <= 0)):
+        if (w <= 0) or (h <= 0) or (x <= 0) or (y <= 0):
             rospy.log("w or h <= 0")
             x = y = w = h = 0
 
-        return image[int(y):int(y+h), int(x):int(x+w)], x, y, w, h
+        return image[int(y):int(y + h), int(x):int(x + w)], x, y, w, h
 
     @staticmethod
     def get_posture_and_gestures(person):
@@ -362,9 +384,9 @@ class PoseEstimator:
         w = color.shape[1]
         h = color.shape[0]
         acquired = self.tf_lock.acquire(False)
-        #cv2.imshow('image', color)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
+        # cv2.imshow('image', color)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
         if not acquired:
             return
 
@@ -372,7 +394,7 @@ class PoseEstimator:
             ts = rospy.Time.now()
             humans = self.humans_to_dict(self.pose_estimator.inference(color, resize_to_default=True,
                                                                        upsample_size=self.resize_out_ratio), w, h)
-            rospy.loginfo('timing tf_pose: %r' % (rospy.Time.now()-ts).to_sec())
+            rospy.loginfo('timing tf_pose: %r' % (rospy.Time.now() - ts).to_sec())
         finally:
             self.tf_lock.release()
 
@@ -388,8 +410,8 @@ class PoseEstimator:
             f_roi, fx, fy, fw, fh = Helper.head_roi(color, human)
             b_roi, bx, by, bw, bh = Helper.upper_body_roi(color, human)
 
-            head_pose = self.helper.depth_lookup(color, depth, fx, fy, fw, fh)
-            body_pose = self.helper.depth_lookup(color, depth, bx, by, bw, bh)
+            person.head_pose_stamped = self.helper.depth_lookup(color, depth, fx, fy, fw, fh)
+            person.pose_stamped = self.helper.depth_lookup(color, depth, bx, by, bw, bh)
 
             face = self.cv_bridge.cv2_to_imgmsg(f_roi, "bgr8")
             faces.append(face)
@@ -420,7 +442,7 @@ class PoseEstimator:
         w = color.shape[1]
         h = color.shape[0]
         persons = self.humans_to_dict(self.pose_estimator.inference(color, resize_to_default=True,
-                                                                       upsample_size=self.resize_out_ratio), w, h)
+                                                                    upsample_size=self.resize_out_ratio), w, h)
         # TODO: filter closest
         return Helper.head_roi(color, persons[0])[0]
 
@@ -439,6 +461,6 @@ class PoseEstimator:
                     person[part] = {'confidence': 0.0,
                                     'x': 0,
                                     'y': 0}
-                # print(person[part])
+                    # print(person[part])
             persons.append(person)
         return persons
