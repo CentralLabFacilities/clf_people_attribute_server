3b8f53eebd3ea261a258feb691b690a830bed97b
Felix Friese <ffriese@techfak.uni-bielefeld.de>
2018-06-11 20:35:33 +0200

    crotch bounding box fix

--- src/detect_people_server.py
+++ src/detect_people_server.py
@@ -409,13 +409,19 @@ class Helper:
             roi.y_offset = person['RightShoulder']['y'] + np.abs(
                 person['RightHip']['y'] - person['RightShoulder']['y']) * 0.6
             roi.width = np.abs(person['RightShoulder']['x'] - person['LeftShoulder']['x'])
-            roi.height = np.abs(person['RightHip']['y'] - person['RightShoulder']['y']) * 0.9
+            roi.height = np.abs(person['RightHip']['y'] - roi.y_offset) * 2
         else:
             roi.x_offset = person['LeftShoulder']['x']
             roi.y_offset = person['LeftShoulder']['y'] + np.abs(
                 person['LeftHip']['y'] - person['LeftShoulder']['y']) * 0.6
             roi.width = np.abs(person['LeftShoulder']['x'] - person['RightShoulder']['x'])
-            roi.height = np.abs(person['LeftHip']['y'] - person['LeftShoulder']['y']) * 0.9
+            roi.height = np.abs(person['LeftHip']['y'] - roi.y_offset) * 2
+
+        if roi.x_offset + roi.width > 640:
+            roi.width = 640 - roi.x_offset
+
+        if roi.y_offset + roi.height > 480:
+            roi.height = 480 - roi.y_offset
 
         # roi.x_offset =
         # roi.y_offset =
